d663027519af06cfa2b279170d41cc62ea4de4cf
Abraham Amani Ndonga <amaniabraham0@gmail.com>
2019-11-29 19:41:15 -0500

    next order of bussiness is to split the files

--- src/ReduxDemo.js
+++ src/ReduxDemo.js
@@ -4,6 +4,28 @@ import { createStore } from 'redux';
 
 class ReduxDemo extends Component {
     render() {
+        // step 2: reducer : state and action (always comes at the top)
+        const reducer = function(state, action) {
+            if(action.type === "ATTACK") {
+                return action.payload
+            }
+            if(action.type === "GREENATTACK"){
+                return action.payload
+            }
+            return state;
+        }
+//step 1 create store: reducer n state
+        const store = createStore(reducer, "Peace");
+        
+        //Step 3: subscribe
+
+        store.subscribe(() => {
+            console.log("Store is now", store.getState());
+        })
+        //Step 4: Dispatch action
+
+        store.dispatch({type: "ATTACK", payload: "Iron Man"})
+        store.dispatch({type: "GREENATTACK", payload: "HULK"})
         return (
             <div>
                 test
