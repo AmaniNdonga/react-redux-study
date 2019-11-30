9f9a851afa51afffba0ae8757fd2dbc5ba9562a7
Abraham Amani Ndonga <amaniabraham0@gmail.com>
2019-11-29 19:09:54 -0500

    created test and cleaned up environment

---
+++ src/ReduxDemo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+
+
+class ReduxDemo extends Component {
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+export default ReduxDemo;
